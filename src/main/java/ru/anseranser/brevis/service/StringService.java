f6cba2581593140f6b1078efaf5c56c597dab4b0
Gusev Sergey Valerevich <sergey.v.gusev@rtmis.ru>
2025-07-27 17:37:27 +0300

    fix sonarcloud error

--- src/main/java/ru/anseranser/brevis/service/StringService.java
+++ src/main/java/ru/anseranser/brevis/service/StringService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
-import java.util.Random;
 
 @Service
 public class StringService {
